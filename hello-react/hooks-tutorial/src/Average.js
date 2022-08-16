cc46229b09ba9e962c028b399b1b8e8fd6dfab88
Youngpro95 <mmyoung95@naver.com>
2022-08-17 02:21:47 +0900

    useRef 학습 null값을 주어 초기화한다.

--- hello-react/hooks-tutorial/src/Average.js
+++ hello-react/hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback,useRef } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중");
@@ -9,20 +9,22 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl=useRef(null);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
+  },[]);
+  const onInsert = useCallback((e) => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
-  };
+    inputEl.current.focus();
+  },[number,list]);
   const avg = useMemo(()=>getAverage(list),[list]);
 
   return (
     <div>
-      <input value={number} onChange={onChange}></input>
+      <input value={number} onChange={onChange} ref={inputEl}></input>
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
