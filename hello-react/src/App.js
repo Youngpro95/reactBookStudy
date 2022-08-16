baba72b366483e0e7e3277b10537c3b4347502f5
Youngpro95 <mmyoung95@naver.com>
2022-08-16 15:00:28 +0900

    keyPress

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Counter from './Counter';
+import EventPractice from "./EventPractice";
 import MyComponent from "./MyComponent";
+import Say from './Say'
 
 const App = () => {
-  return <Counter />;
+  return <EventPractice/>;
+  // return <Say />;
+  // return <Counter />;
   // <MyComponent name="React" favoriteNumber={13}>
   //   리액트
   // </MyComponent>
