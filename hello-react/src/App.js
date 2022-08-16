4d30daa3f37d3f84fa0666c10f43cf014704cb63
Youngpro95 <mmyoung95@naver.com>
2022-08-16 20:16:12 +0900

    react lifeCycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 import ScrollBox from "./ScrollBox";
 import ValidationSample from './ValidationSample';
 
 class App extends Component{
   render(){
     return(
-      <IterationSample/>
+      <LifeCycleSample/>
+      // <IterationSample/>
       // <div>
       //   <ScrollBox ref={(ref)=>{this.ScrollBox=ref}}/>
       //   <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
