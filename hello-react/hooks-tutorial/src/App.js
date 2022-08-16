67f83302234778ba54c12f3cc8e09cf0f76f8b74
Youngpro95 <mmyoung95@naver.com>
2022-08-16 21:49:48 +0900

    reduce 학습

--- hello-react/hooks-tutorial/src/App.js
+++ hello-react/hooks-tutorial/src/App.js
@@ -3,20 +3,21 @@ import Info from "./info";
 import React, { useState } from "react";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  return <Counter/>
 };
 
 export default App;
