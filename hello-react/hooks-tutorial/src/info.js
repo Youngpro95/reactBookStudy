e7c8c0a9c6bbe3ad1b9e4883fb4a967e4240ba26
Youngpro95 <mmyoung95@naver.com>
2022-08-16 21:28:14 +0900

    useEffect 학습

--- hello-react/hooks-tutorial/src/info.js
+++ hello-react/hooks-tutorial/src/info.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from "react";
 
-const Info = ()=>{
-  const [name, setName] = useState('')
-  const [nickname, setNickname] =useState('');
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    // console.log("렌더링이 완료되었습니다!");
+    // console.log({
+    //   name,
+    //   nickname,
+    // });
+    console.log(`effect`);
+    return()=>{
+      console.log('cleanup');
+    }
+  }, []);
 
-  const onChangeName =e =>{
+  const onChangeName = (e) => {
     setName(e.target.value);
-  }
-  const onChangeNickname = e =>{
+  };
+  const onChangeNickname = (e) => {
     setNickname(e.target.value);
-  }
+  };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName}/>
-        <input value={nickname} onChange={onChangeNickname}/>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
       </div>
       <div>
         <div>
@@ -26,7 +37,7 @@ const Info = ()=>{
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Info;
