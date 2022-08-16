afe838f3c8cb2526f9abecd2734e0b2cbf507173
Youngpro95 <mmyoung95@naver.com>
2022-08-16 19:58:51 +0900

    state를 이용하여 C,R,D

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
+import IterationSample from "./IterationSample";
 import ScrollBox from "./ScrollBox";
 import ValidationSample from './ValidationSample';
 
 class App extends Component{
   render(){
     return(
-      <div>
-        <ScrollBox ref={(ref)=>{this.ScrollBox=ref}}/>
-        <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
-      </div>
+      <IterationSample/>
+      // <div>
+      //   <ScrollBox ref={(ref)=>{this.ScrollBox=ref}}/>
+      //   <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      // </div>
       // <ValidationSample/>
     );
   }
