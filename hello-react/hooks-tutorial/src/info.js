2fd2942c5a08d54f9a4f51f8d765591f472a5764
Youngpro95 <mmyoung95@naver.com>
2022-08-18 02:19:43 +0900

    개별 hooks 사용법

--- hello-react/hooks-tutorial/src/info.js
+++ hello-react/hooks-tutorial/src/info.js
@@ -1,26 +1,27 @@
 import React, {useReducer} from 'react';
+import useInputs from './useInputs';
 
-function reducer(state,action){
-  return{
-    ...state,
-    [action.name] : action.value
-  }
-}
+// function reducer(state,action){
+//   return{
+//     ...state,
+//     [action.name] : action.value
+//   }
+// }
 
 const Info =()=>{
-  const [state,dispatch]=useReducer(reducer, {
+  const [state,dispatch]=useInputs({
     name : "",
     nickname:''
   })
   const {name, nickname} = state;
-  const onChange= (e)=>{
-    dispatch(e.target)
-  }
+  // const onChange= (e)=>{
+  //   dispatch(e.target)
+  // }
   return(
     <div>
       <div>
-        <input name="name" value={name} onChange={onChange}/>
-        <input name="nickname" value={nickname} onChange={onChange}/>
+        <input name="name" value={name} onChange={dispatch}/>
+        <input name="nickname" value={nickname} onChange={dispatch}/>
       </div>
       <div>
         <b>이름 : </b> {name}
