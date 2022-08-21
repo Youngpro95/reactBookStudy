1a3540f8ced3d6c4586c19da3372af746a96debf
Youngpro95 <mmyoung95@naver.com>
2022-08-22 01:38:59 +0900

    styledComponent

--- hello-react/styling-react/src/StyledComponent.js
+++ hello-react/styling-react/src/StyledComponent.js
@@ -4,6 +4,14 @@ const Box = styled.div`
   background: ${props => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  width:1024px;
+  margin : 0 auto;
+  @media (max-width : 1024px) {
+    width : 768px;
+  }
+  @media (max-width : 768px){
+    width : 100%;
+  }
 `;
 
 const Button = styled.button`
@@ -23,7 +31,7 @@ const Button = styled.button`
   ${(props) =>
     props.inverted &&
     css`
-      background : white;
+      background : none;
       border : 2px solid white;
       color : white;
       &:hover{
