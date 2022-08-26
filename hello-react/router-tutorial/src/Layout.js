d8d17023928975d5fd5a75d04e96708251ada2d5
Youngpro95 <mmyoung95@naver.com>
2022-08-27 02:08:04 +0900

    useNavigate 학습

--- hello-react/router-tutorial/src/Layout.js
+++ hello-react/router-tutorial/src/Layout.js
@@ -1,15 +1,26 @@
-import { Outlet } from "react-router-dom";
-import React from 'react'
+import { useNavigate, Outlet } from "react-router-dom";
+import React from "react";
 
 export default function Layout() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goArticles = () =>{
+    navigate('/articles', {replace : true});
+  }
+
   return (
     <div>
-      <header style={{background: `lightgray`, padding : 16, fontSize : 24}}>
-        Header
+      <header style={{ background: `lightgray`, padding: 16, fontSize: 24 }}>
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>게시글 목록으로</button>
       </header>
       <main>
-        <Outlet/>
+        <Outlet />
       </main>
     </div>
-  )
+  );
 }
