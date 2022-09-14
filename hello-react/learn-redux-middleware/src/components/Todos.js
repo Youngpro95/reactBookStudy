5fbf0f7e3080ee1ac36b11ba04db0ffd28bfee62
Youngpro95 <mmyoung95@naver.com>
2022-09-14 21:05:50 +0900

    중괄호로 감싸기

--- hello-react/learn-redux-middleware/src/components/Todos.js
+++ hello-react/learn-redux-middleware/src/components/Todos.js
@@ -1,38 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-const TodoItem = React.memo(function TodoItem({todo,onToggle}){
-  return(
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
+  return (
     <li
-    style={{textDecoration : todo.done? 'line-through':'none'}}
-    onClick={()=>onToggle(todo.id)}>{todo.text}</li>
-  )
-})
-const TodoList = React.memo(function TodoList({todos, onToggle}){
-  return(
+      style={{ textDecoration: todo.done ? "line-through" : "none" }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+});
+const TodoList = React.memo(function TodoList({todos,onToggle}){
+  return (
     <ul>
-      {todos.map(todo=>(
+      {todos.map(todo => (
         <TodoItem key={todo.id} todo={todo} onToggle={onToggle}/>
       ))}
     </ul>
   )
 })
 
-export default function Todos(todos, onCreate, onToggle) {
-
-  const [text, setText] = useState('');
-  const onChange = e =>setText(e.target.value);
-  const onSubmit = e =>{
+export default function Todos({todos, onCreate, onToggle}) {
+  const [text, setText] = useState("");
+  const onChange = (e) => setText(e.target.value);
+  const onSubmit = (e) => {
     e.preventDefault();
     onCreate(text);
-    setText('');
-  }
+    setText("");
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={text} placeholder="할 일 입력" onChange={onChange}/>
+        <input value={text} placeholder="할 일 입력" onChange={onChange} />
         <button type="submit">등록</button>
       </form>
-      <TodoList todos={todos} onToggle={onToggle}/>
+      <TodoList todos={todos} onToggle={onToggle} />
     </div>
-  )
+  );
 }
